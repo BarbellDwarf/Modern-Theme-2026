b1ed274ac8bced121e370b259dc7326b00bc28d5
Modern Theme 2026 Team <noreply@github.com>
2026-07-30 16:35:01 +0000

    fix: register console command via config.php consoleControllerMap for reliable discovery

--- Module.php
+++ Module.php
@@ -35,11 +35,6 @@ class Module extends \humhub\components\Module
     {
         parent::init();
         $this->registerTranslations();
-
-        // Register console commands so `php yii modern-theme-2026/update` works
-        if (Yii::$app instanceof \yii\console\Application) {
-            $this->controllerMap['update'] = 'humhub\modules\modernTheme2026\commands\UpdateController';
-        }
     }
 
     /**
--- config.php
+++ config.php
@@ -6,6 +6,7 @@
  */
 
 use humhub\components\View;
+use humhub\modules\modernTheme2026\commands\UpdateController;
 use humhub\modules\modernTheme2026\Events;
 use humhub\widgets\TopMenu;
 use yii\base\Application;
@@ -35,5 +36,9 @@ return [
             'event' => TopMenu::EVENT_RUN,
             'callback' => [Events::class, 'onTopMenuRun'],
         ],
+    ],
+
+    'consoleControllerMap' => [
+        'theme2026-update' => 'humhub\modules\modernTheme2026\commands\UpdateController',
     ],
 ];
